cc51fef8d3df8ba40af94425446fd841ee387b0b
andyphan <andy.phan@infoconnect.com.my>
2015-01-12 11:15:30 +0800

    Mockup screen for Search and User View

---
+++ SearchView.h
@@ -0,0 +1,21 @@
+//
+//  SearchView.h
+//  CliqueProject
+//
+//  Created by Andy Phan on 1/12/15.
+//  Copyright (c) 2015 CoDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <sqlite3.h>
+#import "FMDatabase.h"
+#import "FMResultSet.h"
+
+
+@interface SearchView : UIViewController<UIPickerViewDelegate,UITextFieldDelegate>{
+    
+    
+    
+}
+
+@end
---
+++ SearchView.m
@@ -0,0 +1,34 @@
+//
+//  SearchView.m
+//  CliqueProject
+//
+//  Created by Andy Phan on 1/12/15.
+//  Copyright (c) 2015 CoDeveloper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SearchView.h"
+
+@interface SearchView (){
+    
+    
+}
+
+@end
+@implementation SearchView
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    
+    
+}
+
+
+
+@end
+
+
